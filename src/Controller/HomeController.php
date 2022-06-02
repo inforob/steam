c6275f80605e1e5e427a72cc4b39011f37c90e11
inforob <roberto.manchado@sunmedia.tv>
2022-06-02 16:04:09 +0200

    fist commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="app")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig',[]);
+    }
+}
