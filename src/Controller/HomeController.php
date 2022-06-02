200e79c64a90af0e20d28a6621dfc180065a2123
inforob <roberto.manchado@sunmedia.tv>
2022-06-02 17:29:37 +0200

    feat:SMT-001 - Macros

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +16,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="_home")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render('home/index.html.twig',[]);
+        return $this->render('home/index.html.twig',[
+            'posts' => $postRepository->findBy(['published'=>Post::PUBLISHED])
+        ]);
     }
 }
